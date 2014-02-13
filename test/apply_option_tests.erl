058065257e9c3f244754ed973932259dc90bf7d8
Dmitry Gureev <dmi.gureev@gmail.com>
2014-02-13 16:01:25 +0400

    time test

--- test/apply_option_tests.erl
+++ test/apply_option_tests.erl
@@ -29,12 +29,18 @@ yamerl_parse_time_test_() ->
         ?assertEqual(Res, [[{time, 86399}]])
     end.
 
-yamerl_time_to_erlang_() ->
+yamerl_time_to_erlang_test() ->
     fun() ->
         [Res|_T] = yamerl:decode("time: 23:59:59", ?NODE_OPTION),
-        Time = proplists:get_value(time, Res),
-        ErlTime = calendat:seconds_to_time(Time),
-        ?assertEqual(ErlTime, {23,59,59})
+        {_YMD, HMS} = proplists:get_value(time, Res),
+        ?assertEqual(HMS, {23,59,59})
+    end.
+
+yamerl_time_to_erlang2_test() ->
+    fun() ->
+        [Res|_T] = yamerl:decode("time: 00:00:01", ?NODE_OPTION),
+        {_YMD, HMS} = proplists:get_value(time, Res),
+        ?assertEqual(HMS, {00,00,01})
     end.
 
 stop_app_test_() ->
